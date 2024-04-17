8f737faab66e1797c2667a8a8c4eb15a636af4a5
AnsulChauhan <ansul000chauhan@gmail.com>
2024-04-17 21:41:17 +0530

    Learning maths for DSA

---
+++ MATHS/FadtExponentiation.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int a ,a_power;
+    cout<<"Enter no."<<endl;
+    cin>>a;
+    cout<<"Enter power"<<endl;
+    cin>>a_power;
+    int ans=1;
+    for(int i =0;i<a_power;i++){
+
+        ans =ans*a;
+        // int a = a*a_power;
+
+    }
+
+    cout<<ans;
+    cout<<ans;
+
+
+
+    return 0;
+}
